8ac8a4c4ca68f5f9c0c0dd68f3590cf2da35ae85
Creative15305 <srijanbwl15305@gmail.com>
2025-10-24 23:17:52 +0530

    Implement longest palindrome substring function

---
+++ longestPalindrome.py
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        int n = s.size();
+        if (n == 0) return "";
+        int start = 0, maxLen = 1;
+
+        auto expand = [&](int l, int r) {
+            while (l >= 0 && r < n && s[l] == s[r]) {
+                if (r - l + 1 > maxLen) {
+                    start = l;
+                    maxLen = r - l + 1;
+                }
+                l--; r++;
+            }
+        };
+
+        for (int i = 0; i < n; i++) {
+            expand(i, i);       // Odd length palindrome
+            expand(i, i + 1);   // Even length palindrome
+        }
+
+        return s.substr(start, maxLen);
+    }
+};
